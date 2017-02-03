a725cf111e3da568f45654609ca79a29e3156cef
fredy <fredy@fredy-ThinkPad-E460>
2017-02-03 13:15:51 +0700

    add module biiatm

--- app/code/local/Plus/Biiatm/Block/Form/Biiatm.php
+++ app/code/local/Plus/Biiatm/Block/Form/Biiatm.php
@@ -4,6 +4,7 @@ class Plus_Biiatm_Block_Form_Biiatm extends Mage_Payment_Block_Form
 {
   protected function _construct()
   {
+      
     parent::_construct();
     $this->setTemplate('biiatm/form/biiatm.phtml');
 
